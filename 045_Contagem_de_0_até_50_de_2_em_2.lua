3962b0242f4af0a79b4e7b5d949a692c49f2ab34
Leonardo Cassauara <124696947+leonardocassauara@users.noreply.github.com>
2023-06-07 12:41:39 -0300

    Update

---
+++ 045_Contagem_de_0_até_50_de_2_em_2.lua
@@ -0,0 +1,7 @@
+--[[Problema: Imprima na tela utilizando laços os números inteiros pares de 0 a 50
+    Abstração:  1. for i = 0, 50, 2 coom print
+]]
+
+for i = 0, 50, 2 do
+    io.write(i .. ' ') 
+end
